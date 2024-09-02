c77fca636e57a6470d32423d082b2aef4427abe5
morkworking <morkworking01@gmail.com>
2024-09-02 15:24:48 +0700

    From-main

--- Assets/Scripts/Plate.cs
+++ Assets/Scripts/Plate.cs
@@ -1,7 +1,6 @@
 using System.Collections;
-using System.Collections.Generic;
-using TMPro;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
 
 public class Plate : MonoBehaviour
@@ -16,6 +15,11 @@ public class Plate : MonoBehaviour
     public AudioClip DingSound;
     public AudioClip Endtest;
     public GameObject WrongSe1;
+    public Button CorrectAnswerButton;
+    public Button WrongAnswerButton;
+    public Button CorrectAnswerButton6;
+    public Button WrongAnswerButton6;
+    public test toggleChecker;  // Reference to the `test` script
 
     private AudioSource audioSource;
 
@@ -27,24 +31,48 @@ public class Plate : MonoBehaviour
     private void Start()
     {
         ro1.text = "0.00g";
+        CorrectAnswerButton.onClick.AddListener(HandleCorrectAnswer);
+        WrongAnswerButton.onClick.AddListener(HandleWrongAnswer);
+        CorrectAnswerButton6.onClick.AddListener(HandleCorrectAnswer6);
+        WrongAnswerButton6.onClick.AddListener(HandleWrongAnswer6);
+    }
+
+    private void HandleCorrectAnswer()
+    {
+        Alltoggles[2].GetComponent<Toggle>().isOn = true;  // Assuming the correct toggle is at index 2
+        audioSource.PlayOneShot(DingSound);  // Optional: play a sound for correct answer
+        toggleChecker.Cheak();  // Call the Cheak method to check if all toggles are on
+    }
+    private void HandleCorrectAnswer6()
+    {
+        Alltoggles[5].GetComponent<Toggle>().isOn = true;  // Assuming the correct toggle for question 6 is at index 5
+        audioSource.PlayOneShot(DingSound);  // Optional: play a sound for correct answer
+        toggleChecker.Cheak();  // Call the Cheak method to check if all toggles are on
+    }
+
+    private void HandleWrongAnswer6()
+    {
+        StartCoroutine(ro5sec());
+    }
+    private void HandleWrongAnswer()
+    {
+        StartCoroutine(ro5sec());
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-
         if (collision.gameObject.tag == "Beaker")
         {
             if (Alltoggles[3].GetComponent<Toggle>().isOn == true)
             {
                 Debug.Log("IN");
                 float currentFill = rend.material.GetFloat("_Fill");
-                currentFill = currentFill * 733.4f;
+                currentFill = currentFill * 733f;
                 ro1.text = currentFill.ToString("F0") + ("g");
                 img.color = Color.black;
-                //StartCoroutine(wait());
                 Alltoggles[4].GetComponent<Toggle>().isOn = true;
                 audioSource.PlayOneShot(DingSound);
-                audioSource.PlayOneShot(Endtest);
+                toggleChecker.Cheak();  // Call the Cheak method to check if all toggles are on
             }
             else
             {
@@ -54,19 +82,17 @@ public class Plate : MonoBehaviour
 
         if (collision.gameObject.tag == "Beaker1")
         {
-            if (Alltoggles[1].GetComponent<Toggle>().isOn == true)
+            if (collision.gameObject.tag == "Beaker1")
             {
-                Debug.Log("IN");
-                float currentFill = rend.material.GetFloat("_Fill");
-                currentFill = currentFill * 100f;
-                ro1.text = currentFill.ToString("F0") + ("cm3");
-                img.color = Color.black;
-                Alltoggles[2].GetComponent<Toggle>().isOn = true;
-                audioSource.PlayOneShot(DingSound);
-            }
-            else
-            {
-                StartCoroutine(ro5sec());
+
+                {
+                    Debug.Log("IN");
+                    float currentFill = rend.material.GetFloat("_Fill");
+                    currentFill = currentFill * -100f;
+                    ro1.text = currentFill.ToString("F0") + ("cm3");
+                    img.color = Color.black;
+                }
+
             }
         }
 
@@ -77,8 +103,10 @@ public class Plate : MonoBehaviour
             img.color = Color.black;
             Alltoggles[0].GetComponent<Toggle>().isOn = true;
             audioSource.PlayOneShot(DingSound);
+            toggleChecker.Cheak();  // Call the Cheak method to check if all toggles are on
         }
     }
+
     IEnumerator ro5sec()
     {
         WrongSe1.SetActive(true);
@@ -93,7 +121,6 @@ public class Plate : MonoBehaviour
             Debug.Log("OUT");
             ro1.text = "0.0g";
             img.color = Color.black;
-            //StartCoroutine(wait());
         }
 
         if (collision.gameObject.tag == "Beaker1")
@@ -101,7 +128,6 @@ public class Plate : MonoBehaviour
             Debug.Log("OUT");
             ro1.text = "0.0g";
             img.color = Color.black;
-            //StartCoroutine(wait());
         }
 
         if (collision.gameObject.tag == "Metal")
@@ -109,23 +135,6 @@ public class Plate : MonoBehaviour
             Debug.Log("OUT");
             ro1.text = "0.0g";
             img.color = Color.black;
-            //StartCoroutine(wait());
         }
     }
-
-
-    public IEnumerator wait()
-    {
-        yield return new WaitForSeconds(2f);
-        ro1.text = "0.0g";
-    }
-
-    private void WeightCheck()
-    {
-
-        float currentFill = rend.material.GetFloat("_Fill");
-        rend.material.SetFloat("_Fill", currentFill);
-
-    }
-
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,24 +2,76 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ScoreManager : MonoBehaviour
 {
-    private int scoreAmount;
-    private Text score;
+    public int totalscore;
+    public GameObject panel1, panel2, panel3, panel4, panel5;
+    public GameObject q1, q2, q3, q4, q5;
+    public TMP_Text sumscore;
+    public bool done;
+
+    private HashSet<GameObject> scoredPanels = new HashSet<GameObject>();
+    private HashSet<GameObject> answeredQuestions = new HashSet<GameObject>();
 
     void Start()
     {
-        scoreAmount = 0;
-        score = GetComponent<Text>();
-    }
-    public void Update()
-    {
-        score.text = scoreAmount.ToString();
+        done = false;
+        totalscore = 0;
     }
 
-    public void AddScore()
+    // Update is called once per frame
+    void Update()
     {
-        scoreAmount += 1;
+        CheckPanelActivation(panel1);
+        CheckPanelActivation(panel2);
+        CheckPanelActivation(panel3);
+        CheckPanelActivation(panel4);
+        CheckPanelActivation(panel5);
+        CheckQuestionAnswer(q1);
+        CheckQuestionAnswer(q2);
+        CheckQuestionAnswer(q3);
+        CheckQuestionAnswer(q4);
+        CheckQuestionAnswer(q5);
+
+        sumscore.text = totalscore.ToString() + " / 5";
+    }
+
+    void CheckPanelActivation(GameObject panel)
+    {
+        if (panel.activeSelf && !scoredPanels.Contains(panel))
+        {
+            // Check if the panel should be considered for scoring
+            if (AllPanelsAddressed())
+            {
+                IncreaseScore();
+                scoredPanels.Add(panel);
+            }
+        }
+    }
+
+    void CheckQuestionAnswer(GameObject question)
+    {
+        if (question.activeSelf)
+        {
+            answeredQuestions.Add(question);
+            if (done)
+            {
+                done = false;
+            }
+        }
+    }
+
+    void IncreaseScore()
+    {
+        totalscore = Mathf.Min(totalscore + 1, 5); // Ensure the score doesn't exceed the number of questions
+        done = true;
+    }
+
+    bool AllPanelsAddressed()
+    {
+        // Ensure all questions are answered
+        return answeredQuestions.Count == 5;
     }
 }
